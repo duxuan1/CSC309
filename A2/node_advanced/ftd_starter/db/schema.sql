b3a39cee072a08b262f47c46033395d396e296bb
Donchka <mhd993065259@gmail.com>
2021-03-16 21:18:02 -0400

    login and register

--- A2/node_advanced/ftd_starter/db/schema.sql
+++ A2/node_advanced/ftd_starter/db/schema.sql
@@ -3,9 +3,12 @@
 DROP TABLE ftduser;
 CREATE TABLE ftduser (
 	username VARCHAR(20) PRIMARY KEY,
-	password BYTEA NOT NULL
+	password BYTEA NOT NULL,
+	birthday DATE NOT NULL,
+	skill VARCHAR(20) NOT NULL,
+	prefer_time TEXT[]
 );
 --- Could have also stored as 128 character hex encoded values
 --- select char_length(encode(sha512('abc'), 'hex')); --- returns 128
-INSERT INTO ftduser VALUES('user1', sha512('password1'));
-INSERT INTO ftduser VALUES('user2', sha512('password2'));
+INSERT INTO ftduser VALUES('user1', sha512('password1'), '1998-05-25', 'beginner');
+INSERT INTO ftduser VALUES('user2', sha512('password2'), '1998-05-21', 'intermediate');
